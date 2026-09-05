9dc7fa15701badff391ce30b9d1b90753ed9eb43
Mohammed-163 <monaoeet@gmail.com>
2026-09-05 23:21:53 +0300

    Update triggers.sql

--- phase5_6/database/triggers.sql
+++ phase5_6/database/triggers.sql
@@ -5,6 +5,14 @@
 -- (migration_manager applies schema.sql -> indexes.sql -> functions.sql ->
 -- triggers.sql -> views.sql, in that order).
 
+CREATE OR REPLACE FUNCTION set_updated_at()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
 DROP TRIGGER IF EXISTS trg_posts_set_updated_at ON posts;
 CREATE TRIGGER trg_posts_set_updated_at
     BEFORE UPDATE ON posts
