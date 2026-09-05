41e3ae96b4aee03284bd33892f442fddb16e5b30
Mohammed-163 <monaoeet@gmail.com>
2026-09-05 14:53:56 +0300

    Update wiring.py

--- phase5_6/core/wiring.py
+++ phase5_6/core/wiring.py
@@ -4,7 +4,6 @@ import os
 import sys
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
 from core.container import container
-from core.wiring import wire_default_subscribers
 from lib import config
 from lib.telegram_notifier import TelegramNotifier
 
